a39dfabb6908180f8d166bb47eb1cbb9103ee6dc
Rayyan Nadeem <rayyanayan786@gmail.com>
2023-10-17 14:15:25 -0400

    updated

--- query_enterprise_attack_data_updated.py
+++ query_enterprise_attack_data_updated.py
@@ -1,64 +1,67 @@
-import pandas as pd
 import json
+
+# File paths
+source_data_file_path = "MIPSA TM Blank.json"
+db_data_file_path = "enterprise-attack-v13.1.json"
+query_file_path = "param.json"
 
 def load_data(filename):
     with open(filename, 'r', encoding='utf-8-sig') as f:
         return json.load(f)
 
+def append_threat_to_entity(source_data, entity_id, threats):
+    for diagram in source_data.get('detail', {}).get('diagrams', []):
+        for cell in diagram.get('cells', []):
+            if cell.get('id') == entity_id:
+                cell_data = cell.get('data', {})
+                current_threats = cell_data.get('threats', [])
+                current_threats.extend(threats)
+                cell_data['threats'] = current_threats
+                break
+    return source_data
+
 def query_data(data, queries):
     results = []
-    matched_ids = set()  # Track IDs of matched techniques to prevent duplicates
-
-    # Convert comma-separated string values to lists for "match any" behavior
+    matched_ids = set()
     for key, value in queries.items():
         if isinstance(value, str) and ',' in value:
             queries[key] = [v.strip() for v in value.split(',')]
-
-    # Find matching techniques based on queries
     for item in data:
         match = True
         for key, value in queries.items():
             item_values = item.get(key, []) or []
-            if not value:  # If the query value is None or empty, it's a wildcard match
+            if not value:
                 continue
-            elif not item_values:  # If the item value is None or empty, it's applicable to all
+            elif not item_values:
                 continue
-            elif isinstance(value, list):  # Special handling for "match any" behavior
-                if not any(v in item_values for v in value):  # Check if any of the query values match
+            elif isinstance(value, list):
+                if not any(v in item_values for v in value):
                     match = False
                     break
-            elif isinstance(item_values, list):  # For fields with list values
-                if not any(value in v for v in item_values):  # Check for partial matches within the list
+            elif isinstance(item_values, list):
+                if not any(value in v for v in item_values):
                     match = False
                     break
-            elif item.get(key) != value:  # For fields with string values
+            elif item.get(key) != value:
                 match = False
                 break
-
         if match and item['ID'] not in matched_ids:
             results.append(item)
             matched_ids.add(item['ID'])
-            
     return results
 
-# Additional function to load the queries from param.json
-def load_queries(filename):
-    with open(filename, 'r', encoding='utf-8-sig') as f:
-        return json.load(f)
-
-# Updated main block to use the load_queries function
 if __name__ == "__main__":
-    data = load_data("enterprise-attack-v13.1.json")
-    
-    # Load your queries from param.json
-    queries = load_queries("param.json")
+    source_data = load_data(source_data_file_path)
+    db_data = load_data(db_data_file_path)
+    threat_query = load_data(query_file_path)
 
-    # Execute the query and get the results
-    results = query_data(data, queries)
-    
-    # Convert results to a DataFrame and save to a CSV
-    df = pd.DataFrame(results)
-    df.to_csv("query_results.csv", index=False)
+    entity_id = threat_query.pop("EntityID")
+    filtered_techniques = query_data(db_data, threat_query)
 
-    # Print results
-    print(df)
+    updated_data = append_threat_to_entity(source_data, entity_id, filtered_techniques)
+    
+    
+    with open('UPDATED_MIPSA_TM.json', 'w') as updated_file:
+        json.dump(updated_data, updated_file, indent=4)
+    
+    print(f"{len(filtered_techniques)} techniques appended successfully!")
